dc5dfe3ac1a797fa0655d19eaec25e1ceeb99b23
Rachaelfavour <60661756+Rachaelfavour@users.noreply.github.com>
2020-05-29 18:31:37 +0100

    Add files via upload

---
+++ Features/PostItem.feature
@@ -0,0 +1,25 @@
+﻿Feature: PostItem
+	In order to be a guru tester
+	I want to automate the post item page
+	Which has lots of features to automate in future
+
+
+@mytag
+Scenario: Verify Post Item
+Given I navigate to the site "https://www.qa.giftrete.com"
+    And I click on Signin
+	When I enter Emailaddress 
+	And I enter Password 
+	And I click on Signin button
+	And I click on PostItem
+	And I enter ItemName
+	And I enter ItemDescription 
+	And I select ItemCategory
+	And I select ItemType 
+	And I enter ItemWeight
+	And I enter Location 
+	And I upload image
+	And I click on add item
+	Then Item image is displayed
+
+	
---
+++ PageObjects/PostItemPage.cs
@@ -0,0 +1,184 @@
+﻿using GiftreteWebsitePostItem_Project.Hooks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GiftreteWebsitePostItem_Project.PageObjects
+{
+    class PostItemPage
+    {
+        IWebDriver driver;
+
+
+        public PostItemPage()
+        { 
+            driver = Hook.driver;
+        }  
+
+        IWebElement SignIn => driver.FindElement(By.XPath("//a[text()='Sign In']"));
+
+        IWebElement EmailAddress => driver.FindElement(By.XPath("//input[@name = 'user_email']"));
+
+        IWebElement Password => driver.FindElement(By.XPath("//input[@id='user_password']"));
+
+        IWebElement SignInButton => driver.FindElement(By.CssSelector("#btn_signin"));
+
+        IWebElement Postitem => driver.FindElement(By.CssSelector("a[href='/post/create']"));
+
+        IWebElement ItemName => driver.FindElement(By.XPath("//input[@id = 'title']"));
+
+        IWebElement ItemDescription => driver.FindElement(By.XPath("/html"));
+
+        IWebElement SelectFurnitureItemCategory => driver.FindElement(By.XPath("//*[@id='categoryone']"));
+
+        IWebElement OffereditemType => driver.FindElement(By.XPath("//select[@id = 'type']"));
+
+        IWebElement ItemWeight => driver.FindElement(By.XPath("//input[@id = 'weight']"));
+
+        IWebElement Location => driver.FindElement(By.XPath("//input[@id = 'autocomplete']"));
+
+        IWebElement UploadItem => driver.FindElement(By.XPath("//*[@id='files']"));
+
+        IWebElement AddItem => driver.FindElement(By.XPath("//*[@id='btn_create']"));
+
+        
+
+        public void EnterLocation()
+        {
+            Location.SendKeys("London, uk");
+        }
+
+
+        public void EnterItemWeight()
+        {
+            ItemWeight.SendKeys("25kg");
+        }
+
+        public void SelectOfferedFromItemType()
+        {
+            SelectElement select = new SelectElement(OffereditemType);
+            select.SelectByValue("Offered");
+        }
+
+
+        public void SelectFurnitureFromItemCategory()
+        {
+            SelectElement select = new SelectElement(SelectFurnitureItemCategory);
+            select.SelectByValue("4");
+
+        }   
+
+        public void EnterItemDescription()
+        {
+            ItemDescription.SendKeys("Armchair for your lounge");
+        }
+
+        public void EnterItemName()
+        {
+            ItemName.SendKeys("Chair");
+        }
+
+        public void ClickOnPostItem()
+        {
+            Postitem.Click();
+        }
+
+
+        public void ClickOnSignInButton()
+        {
+            SignInButton.Click();
+        }
+
+        internal void UploadImage()
+        {
+            UploadItem.SendKeys("C:\\Users\\Favour\\Desktop\\Armchair.png");
+        }
+
+        public void EnterPassword()
+        {
+            Password.SendKeys("favour123");
+        }
+
+        internal void ClickAddItem()
+        {
+            AddItem.Click();
+        }
+
+        public void EnterEmailAddess()
+        {
+            EmailAddress.SendKeys("richkome@yahoo.com");
+        }
+        public void ClickOnSignIn()
+        {
+            SignIn.Click();
+        }
+        
+        public void NavigateToWebsite(string url)
+        {
+            driver.Navigate().GoToUrl(url);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
