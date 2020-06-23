a7ea7a2e0dd862e03aa0230955948400c9424f8c
Rachaelfavour <60661756+Rachaelfavour@users.noreply.github.com>
2020-06-23 17:44:09 +0100

    Add files via upload
    API testing on Giftrete website

---
+++ BasicAPISteps.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using RestSharp;
+using System;
+using TechTalk.SpecFlow;
+
+namespace GiftreteWebsitePostItem_Project.StepDefinitions
+{
+    [Binding]
+    public class BasicAPISteps
+    {
+        RestClient client;
+        RestRequest request;
+        IRestResponse response;
+
+        [Given(@"I navigate to Github")]
+        public void GivenINavigateToGithub()
+        {
+            client = new RestClient("https://github.com/Rachaelfavour");
+            client.Timeout = -1;
+           
+        }
+        
+        [When(@"I search for user")]
+        public void WhenISearchForUser()
+        {
+            request = new RestRequest(Method.GET);
+            request.AddHeader("Cookie", "_octo=GH1.1.1330731310.1592847096; logged_in=no; _gh_sess=10LuNKnTNe03%2BeDbdod5jMlMGMsFCKSXlfgTiCKAXqGOzpX4fHrzcKQ%2F5Jl%2FtGB%2FtLJYir54jpsJTZVnmXfUc4vT7eBUireoIhWfJN7VIEoImTO2pRDYbx%2F684UpY6q95LjpFbh64MR1poJzVZ1m5%2F7SQCBxHAuBrx66s2J8KwOnrNDop3LcwRC7QAzpvaQ8yzxAYy8GYEWSYQzb%2B36xcgEBm3xUYGHSzZ8VutqcahQxJSdFgWgwJo%2FT4rOoWpqeu6wlOoi%2F2cWmBJKht00niQ%3D%3D--8yskppSBdhM%2BXfbO--LzcALBvk1jhSBfq9KIMh%2BQ%3D%3D");
+            response = client.Execute(request);
+           
+        }
+        
+        [Then(@"result is displyed")]
+        public void ThenResultIsDisplyed()
+        {
+            Assert.That(response.Content.Length > 0);
+        }
+    }
+}
---
+++ PostItemSteps.cs
@@ -0,0 +1,113 @@
+﻿using GiftreteWebsitePostItem_Project.PageObjects;
+using NUnit.Framework;
+using System;
+using TechTalk.SpecFlow;
+
+namespace GiftreteWebsitePostItem_Project.StepDefinitions
+{
+    [Binding]
+    public class PostItemSteps
+    {
+        PostItemPage postItempage;
+        public PostItemSteps()
+        {
+            postItempage = new PostItemPage();
+        }
+
+        [Given(@"I click on Signin")]
+        public void GivenIClickOnSignin()
+        {
+            postItempage.ClickOnSignIn();
+        }
+
+        [When(@"I click on Signin button")]
+        public void WhenIClickOnSigninButton()
+        {
+            postItempage.ClickOnSignInButton();
+        }
+
+        [Given(@"I navigate to the site ""(.*)""")]
+        public void GivenINavigateToTheSite(string url)
+        {
+            postItempage.NavigateToWebsite(url);
+        }
+        
+        [When(@"I enter Emailaddress")]
+        public void WhenIEnterEmailaddress()
+        {
+            postItempage.EnterEmailAddess();
+        }
+        
+        [When(@"I enter Password")]
+        public void WhenIEnterPassword()
+        {
+            postItempage.EnterPassword();
+        }
+        
+        
+        [When(@"I click on PostItem")]
+        public void WhenIClickOnPostItem()
+        {
+            postItempage.ClickOnPostItem();
+        }
+        
+        [When(@"I enter ItemName")]
+        public void WhenIEnterItemName()
+        {
+            postItempage.EnterItemName();
+
+        }
+        
+        [When(@"I enter ItemDescription")]
+        public void WhenIEnterItemDescription()
+        {
+            postItempage.EnterItemDescription();
+        }
+        
+        [When(@"I select ItemCategory")]
+        public void WhenISelectItemCategory()
+        {
+         postItempage.SelectFurnitureFromItemCategory();
+        }
+        
+        [When(@"I select ItemType")]
+        public void WhenISelectItemType()
+        {
+            postItempage.SelectOfferedFromItemType();
+        }
+        
+        [When(@"I enter ItemWeight")]
+        public void WhenIEnterItemWeight()
+        {
+            postItempage.EnterItemWeight();
+        }
+        
+        [When(@"I enter Location")]
+        public void WhenIEnterLocation()
+        {
+            postItempage.EnterLocation();
+        }
+
+        [When(@"I upload image")]
+        public void WhenIUploadImage()
+        {
+            postItempage.UploadImage();
+        }
+
+        [When(@"I click on add item")]
+        public void WhenIClickOnAddItem()
+        {
+            postItempage.ClickAddItem();
+        }
+
+        [Then(@"Item image is displayed")]
+        public void ThenItemImageIsDisplayed()
+        {
+           // Assert.IsTrue(postItempage.itemImageIsDisplayed());
+        }
+
+
+
+
+    }
+}
